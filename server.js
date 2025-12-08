c46114e04e0f1d7e6d91d46aad1a4cecd2d549d5
root <root@jiawei.ltd>
2025-12-08 02:41:53 -0500

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
  
-app.use('/public', express.static('public'));
+// app.use('/public', express.static('public'));
  
 app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
